2174001aa886fc92ababad2713ffdea28e6a6d19
unknown <rhyz@xtendly.com>
2023-07-30 00:41:58 +0800

    added user related state

--- src/stores/user.tsx
+++ src/stores/user.tsx
@@ -1,5 +1,5 @@
 import { FC, createContext, useState, useEffect } from "react";
-import { authAPI } from "../api";
+import { HttpMethod, authAPI, userAPI } from "../api";
 import { useNavigate } from "react-router-dom";
 
 export interface IUser {
@@ -10,9 +10,12 @@ export interface IUser {
 }
 
 export interface IAuthContext {
-  user: IUser | null;
-  login: (body: unknown) => void;
+  user?: IUser | null;
   loading: boolean;
+  responseMessage: string;
+  login: (body: unknown) => void;
+  register: (body: unknown) => void;
+  setupProfile: (body: any) => void;
 }
 
 export const AuthContext = createContext<IAuthContext>(
@@ -23,33 +26,114 @@ const AuthContextProvider: FC<{ children: any }> = ({ children }) => {
   const navigate = useNavigate();
 
   const [loading, setLoading] = useState<boolean>(false);
-  const [user, setUser] = useState<IUser>({
-    email: "",
-    name: "",
-    course: "",
-    position: "",
-  });
+  const [responseMessage, setResponseMessage] = useState<string>("");
+  const [user, setUser] = useState<IUser>();
 
   useEffect(() => {
-    if (!localStorage.getItem("accessToken")) {
-      navigate("/login");
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    getUserInfo();
   }, []);
 
-  const login = async (body: any) => {
-    setLoading(true);
-    const response: any = await authAPI.login(body);
-    if (response.payload?.access_token) {
-      localStorage.setItem("accessToken", response.payload.access_token);
-      setUser(response.payload.profile);
-      navigate("/");
+  const getUserInfo = async () => {
+    try {
+      const response = await authAPI.call(HttpMethod.GET, `/user`);
+
+      if (response.statusCode >= 400 || !response) {
+        console.log("here1", response);
+        localStorage.removeItem("accessToken");
+        navigate("/auth/login");
+      }
+      if (response?.profile === null) {
+        console.log("here", response);
+        setUser(response.profile);
+        navigate("/user/setup-profile");
+      }
+    } catch (error) {
+      localStorage.removeItem("accessToken");
+      navigate("/auth/login");
     }
-    setLoading(false);
+  };
+
+  const login = async (body: any) => {
+    try {
+      setLoading(true);
+      const response: any = await authAPI.login(body);
+      if (response?.payload) {
+        localStorage.setItem("accessToken", response.payload.access_token);
+        setUser(response.payload.profile);
+        // navigate("/");
+        if (response.payload.profile === null) {
+          navigate("/user/setup-profile");
+        } else {
+          navigate("/");
+        }
+      } else {
+        setResponseMessage(response.message);
+        setTimeout(() => {
+          setResponseMessage("");
+        }, 2000);
+      }
+    } catch (error: any) {
+      setResponseMessage("Server Error");
+      setTimeout(() => {
+        setResponseMessage("");
+      }, 2000);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const setupProfile = async (body: any) => {
+    try {
+      setLoading(true);
+      const response = await userAPI.setupProfile(body);
+      if (response?.payload) {
+        // localStorage.setItem("accessToken", response.payload.access_token);
+        navigate("/");
+        if (response.payload.profile === null) {
+          navigate("/register");
+        } else {
+          setUser(response.payload.profile);
+          navigate("/");
+        }
+      } else {
+        setResponseMessage(response.message);
+        setTimeout(() => {
+          setResponseMessage("");
+        }, 2000);
+      }
+    } catch (error) {
+      setResponseMessage("Server Error");
+      setTimeout(() => {
+        setResponseMessage("");
+      }, 2000);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const register = async (body: any) => {
+    try {
+      setLoading(true);
+      const data = await authAPI.register(body);
+      if (!data.error) {
+        localStorage.setItem("accessToken", data.payload.access_token);
+        navigate("/user/setup-profile");
+      } else {
+        setTimeout(() => {
+          setResponseMessage("");
+        }, 2000);
+        setResponseMessage(data.message);
+      }
+    } catch (error) {
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <AuthContext.Provider value={{ user, loading, login }}>
+    <AuthContext.Provider
+      value={{ user, loading, responseMessage, login, register, setupProfile }}
+    >
       {children}
     </AuthContext.Provider>
   );
