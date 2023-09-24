98fe9c0dad5ee258e4fc78810c3ab061333535a0
unknown <rhyz@xtendly.com>
2023-09-24 13:22:16 +0800

    implemented hook for uploading assets (image only yet)

---
+++ src/hooks/useUploadImage.tsx
@@ -0,0 +1,68 @@
+import { ChangeEvent, useContext, useState } from "react";
+import { MessageContext } from "../stores/message";
+import { assetsAPI } from "../api/assets";
+
+interface IUploadedAssetHook {
+  onAddFile: (image: IUploadedImage) => void;
+}
+
+export interface IUploadedImage {
+  createdAt: string;
+  id: number;
+  publicId: string;
+  url: string;
+}
+
+export const useUploadAsset = () => {
+  const messageContext = useContext(MessageContext);
+  const [uploadedFile, setUploadedFile] = useState<IUploadedImage>();
+
+  const onCheckDimensions = (file: File) => {
+    return new Promise((resolve, reject) => {
+      const toBeCheckedImage = new Image();
+      const url = URL.createObjectURL(file);
+      toBeCheckedImage.src = url;
+      let width;
+      let height;
+      toBeCheckedImage.onload = async () => {
+        width = toBeCheckedImage.naturalWidth || toBeCheckedImage.width;
+        height = toBeCheckedImage.naturalHeight || toBeCheckedImage.height;
+        if (Number.parseInt((width % height).toFixed(1)) === 0) {
+          resolve(true);
+        } else {
+          messageContext.onAddMessage("Image should have 1:1 ratio");
+          reject(false);
+        }
+      };
+    });
+  };
+
+  const onUploadFile = async (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const selectedFile = event.target.files[0];
+      const toBeCheckedImage = new Image();
+      const url = URL.createObjectURL(selectedFile);
+      toBeCheckedImage.src = url;
+
+      try {
+        const res = await onCheckDimensions(selectedFile);
+        if (res) {
+          const imageFormData = new FormData();
+          imageFormData.append("image", selectedFile);
+          const data = await assetsAPI.uploadAsset(imageFormData, true);
+          setUploadedFile((_prevUploaded) => ({
+            ...data,
+          }));
+          // onAddFile(uploadedFile);
+        }
+      } catch (error) {
+        return;
+      }
+    }
+  };
+
+  return {
+    uploadedFile,
+    onUploadFile,
+  } as const;
+};
