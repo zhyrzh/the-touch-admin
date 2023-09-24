93afb158bf61568200b5e36202e64b5372159c7a
unknown <rhyz@xtendly.com>
2023-09-24 13:27:41 +0800

    modified to cater redirection related to authentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,17 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../stores/user";
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+  const authContext = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (authContext.user === null) {
+      navigate("/user/setup-profile");
+    }
+  }, [authContext.user]);
+
   return (
     <div>
       <h1>Main page</h1>
