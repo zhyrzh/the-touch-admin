337a426b5bdb6207ed684e3a136ce02f96b692b3
unknown <rhyz@xtendly.com>
2023-09-24 13:19:08 +0800

    modification to cater items that has body as FormData

--- src/api/root.ts
+++ src/api/root.ts
@@ -8,42 +8,57 @@ export enum HttpMethod {
 const BASE_API = "http://localhost:8000";
 
 class API {
-  async call(method: HttpMethod, url: string, body?: any) {
+  static async call(
+    method: HttpMethod,
+    url: string,
+    body?: any,
+    isFormData: boolean = false
+  ) {
     const ACCESS_TOKEN = localStorage.getItem("accessToken");
+    let headers: HeadersInit = {
+      Authorization: `Bearer ${ACCESS_TOKEN}`,
+    };
+
+    if (!isFormData) {
+      headers = {
+        ...headers,
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      };
+    }
+
     try {
       const response: Response = await fetch(`${BASE_API}${url}`, {
         method,
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          Authorization: `Bearer ${ACCESS_TOKEN}`,
-        } as HeadersInit,
-        body: JSON.stringify(body),
+        headers,
+        body: !isFormData ? JSON.stringify(body) : body,
       });
-      // console.log(await response.json(), "check response");
-      // if (response.ok) {
-      //   return await response.json();
-      // }
-      return await response.json();
+
+      if (!response.ok) {
+        return Promise.reject(await response.json());
+      }
+
+      return Promise.resolve(await response.json());
     } catch (error: any) {
+      console.log("error");
       return Promise.reject(error);
     }
   }
 
-  async post<T>(url: string, body: T) {
-    return await this.call(HttpMethod.POST, url, body);
+  static async post<T>(url: string, body: T, isFormData: boolean = false) {
+    return await API.call(HttpMethod.POST, url, body, isFormData);
   }
 
-  async get<T>(url: any, body: T) {
-    return await this.call(HttpMethod.GET, url, body);
+  static async get<T>(url: any, body?: T) {
+    return await API.call(HttpMethod.GET, url, body);
   }
 
-  async put<T>(url: any, body: T) {
+  static async put<T>(url: any, body: T) {
     console.log(url, body);
     return "get req response";
   }
 
-  async delete<T>(url: any, body: T) {
+  static async delete<T>(url: any, body: T) {
     console.log(url, body);
     return "get req response";
   }
