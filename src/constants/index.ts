9d1298e557f5f555d218e66812358597b58913af
unknown <rhyz@xtendly.com>
2023-09-29 21:53:26 +0800

    added array constants for dropdown input values

---
+++ src/constants/index.ts
@@ -0,0 +1,41 @@
+export const courseOptions = [
+  { key: "cpe", name: "BS Computer Engineering" },
+  { key: "ce", name: "BS Civil Engineering" },
+  {
+    key: "ece",
+    name: "BS Electronics and Communications Engineering",
+  },
+  {
+    key: "ee",
+    name: "BS Electrical Engineering",
+  },
+  {
+    key: "gde",
+    name: "BS Geodetic Engineering",
+  },
+  {
+    key: "me",
+    name: "BS Mechanical Engineering",
+  },
+  {
+    key: "ge",
+    name: "BS Geothermal Engineering",
+  },
+  {
+    key: "arki",
+    name: "BS Architecture",
+  },
+];
+
+export const posOptions = [
+  { key: "news-writer", name: "News Writer" },
+  { key: "feature-writer", name: "Feature Writer" },
+  {
+    key: "photojournalist",
+    name: "Photojournalist",
+  },
+  {
+    key: "sports-writer",
+    name: "Sports Writer",
+  },
+];
