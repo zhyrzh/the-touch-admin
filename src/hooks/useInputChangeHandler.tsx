fed7daae25692aebac24239248a0c446d13fed54
unknown <rhyz@xtendly.com>
2023-10-15 12:48:31 +0800

    added quill change and time change methods for handling change event

--- src/hooks/useInputChangeHandler.tsx
+++ src/hooks/useInputChangeHandler.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, useState } from "react";
 import { IDropdownInputOption } from "../components/input/DropdownInput";
+import dayjs from "dayjs";
 
 const useInputChangeHandler = <T,>() => {
   const [data, setData] = useState<T>();
@@ -23,10 +24,26 @@ const useInputChangeHandler = <T,>() => {
     }));
   };
 
+  const onQuillChange = (value: string, name: string) => {
+    setData((prevData: any) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const onInputTimeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setData((prevData: any) => ({
+      ...prevData,
+      [e.target.name]: dayjs(e.target.value).format("YYYY-MM-DD HH:mm:ss"),
+    }));
+  };
+
   return {
     data,
     onInputChangeHandler,
     onDropdownInputChangeHandler,
+    onQuillChange,
+    onInputTimeChange,
   };
 };
 
