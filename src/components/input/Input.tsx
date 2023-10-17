5707288b04e151ad53cb194d9b7e729238a5940a
unknown <rhyz@xtendly.com>
2023-10-17 21:39:10 +0800

    added same props from TextInput component

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,23 +1,59 @@
-import { FC, useRef } from "react";
-
+import { FC, useRef, ChangeEventHandler } from "react";
+import { IError } from "../../utils";
 interface IInput {
+  name: string;
   label: string;
-  type?: "text" | "datetime-local";
+  value: string;
+  errors: Array<IError>;
+  isPassword?: boolean;
+  onInputChangeHandler: ChangeEventHandler;
+  removeErrors: (name: string) => void;
 }
 
-const Input: FC<IInput> = ({ label, type = "text" }) => {
+const Input: FC<IInput> = ({
+  label,
+  name,
+  errors,
+  value,
+  isPassword = false,
+  onInputChangeHandler,
+  removeErrors,
+}) => {
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const onChangeHandler: ChangeEventHandler<HTMLInputElement> = (event) => {
+    if (errors.findIndex((err) => err.for === name) > -1) {
+      removeErrors(name);
+    }
+    onInputChangeHandler(event);
+  };
+
+  const onCheckErrors = (): React.CSSProperties | undefined => {
+    return errors.findIndex((err) => err.for === name) > -1
+      ? { border: "1px solid red" }
+      : undefined;
+  };
+
   return (
-    <div
-      className="text-input"
-      onClick={() => {
-        if (type === "datetime-local" && inputRef.current) {
-          inputRef.current.showPicker();
-        }
-      }}
-    >
+    <div className="text-input">
       <p className="text-input__label">{label}</p>
-      <input className="text-input__input" type={type} ref={inputRef} />
+      <input
+        type={!isPassword ? "text" : "password"}
+        className="text-input__input"
+        onChange={onChangeHandler}
+        value={value}
+        name={name}
+        ref={inputRef}
+        style={onCheckErrors()}
+      />
+      {errors.findIndex((err) => err.for === name) > -1 ? (
+        <p
+          className="auth__condition-text"
+          style={{ marginTop: "2px", color: "red" }}
+        >
+          {errors[errors.findIndex((err) => err.for === name)].message}
+        </p>
+      ) : null}
     </div>
   );
 };
