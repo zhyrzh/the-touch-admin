3e8ad6218dfbb6360a70f0b49b8c9ffd2dc43964
unknown <rhyz@xtendly.com>
2023-10-18 19:06:42 +0800

    separted uploading for profile image and standard assets

--- src/api/assets.ts
+++ src/api/assets.ts
@@ -1,6 +1,15 @@
 import API from "./root";
 
 class AssetsAPI extends API {
+  async uploadProfileAsset(body: any, isFileUpload: boolean = false) {
+    const response = await API.post(
+      "/upload-library/profile-image",
+      body,
+      isFileUpload
+    );
+    return response;
+  }
+
   async uploadAsset(body: any, isFileUpload: boolean = false) {
     const response = await API.post(
       "/upload-library/image",
