fedfd9662c147045929e3454d5ccbf41646fb802
zhyrzh <arongrhyz@gmail.com>
2023-10-24 08:25:21 +0800

    modified key for dropdown options to match required type

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,45 +1,45 @@
 export const courseOptions = [
-  { key: "cpe", name: "BS Computer Engineering" },
-  { key: "ce", name: "BS Civil Engineering" },
+  { key: "cpe", label: "BS Computer Engineering" },
+  { key: "ce", label: "BS Civil Engineering" },
   {
     key: "ece",
-    name: "BS Electronics and Communications Engineering",
+    label: "BS Electronics and Communications Engineering",
   },
   {
     key: "ee",
-    name: "BS Electrical Engineering",
+    label: "BS Electrical Engineering",
   },
   {
     key: "gde",
-    name: "BS Geodetic Engineering",
+    label: "BS Geodetic Engineering",
   },
   {
     key: "me",
-    name: "BS Mechanical Engineering",
+    label: "BS Mechanical Engineering",
   },
   {
     key: "ge",
-    name: "BS Geothermal Engineering",
+    label: "BS Geothermal Engineering",
   },
   {
     key: "arki",
-    name: "BS Architecture",
+    label: "BS Architecture",
   },
 ];
 
 export const posOptions = [
-  { key: "news-writer", name: "News Writer" },
-  { key: "feature-writer", name: "Feature Writer" },
+  { key: "news-writer", label: "News Writer" },
+  { key: "feature-writer", label: "Feature Writer" },
   {
     key: "photojournalist",
-    name: "Photojournalist",
+    label: "Photojournalist",
   },
   {
     key: "graphics-artist",
-    name: "Graphics Artist",
+    label: "Graphics Artist",
   },
   {
     key: "sports-writer",
-    name: "Sports Writer",
+    label: "Sports Writer",
   },
 ];
