ae4543846738e02e59abf1f98d6586a9cdda63b4
zhyrzh <arongrhyz@gmail.com>
2023-11-18 22:19:58 +0800

    added alert dialog component with styling

---
+++ src/components/UI/AlertDialog.tsx
@@ -0,0 +1,19 @@
+const AlertDialog = () => {
+  return (
+    <div className="alert-dialog-backdrop">
+      <div className="alert-dialog">
+        <h1 className="alert-dialog__title">Accept article?</h1>
+        <p className="alert-dialog__description">
+          Are you sure you want to accept selected article? Accepting this
+          article will make it visible to the readers of the site.
+        </p>
+        <div className="alert-dialog__button-container">
+          <button>Cancel</button>
+          <button>Accept</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AlertDialog;
