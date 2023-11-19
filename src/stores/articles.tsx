2aa0a50f49fd05e7d43eadc58ef38ba5fd807ccd
zhyrzh <arongrhyz@gmail.com>
2023-11-19 21:19:35 +0800

    added new method for accepting an article

--- src/stores/articles.tsx
+++ src/stores/articles.tsx
@@ -24,6 +24,7 @@ interface IArticleDetails {
 interface IArticleContext {
   articles: Array<IArticleDetails>;
   createArticle: (body: IArticleDetails) => void;
+  acceptArticle: (id: number) => any;
 }
 
 export const ArticleContext = createContext<IArticleContext>(
@@ -41,10 +42,9 @@ const ArticleContextProvider: FC<{ children: any }> = ({ children }) => {
   const getOneArticle = () => {};
 
   const createArticle: (body: IArticleDetails) => void = async (body) => {
-    console.log("here");
     try {
       loadingContext.setIsLoading(true);
-      const data = await articleAPI.createArticle({
+      await articleAPI.createArticle({
         ...body,
         author: body.author.map(({ key, label }) => ({
           email: key,
@@ -71,10 +71,24 @@ const ArticleContextProvider: FC<{ children: any }> = ({ children }) => {
     }
   };
 
+  const acceptArticle = async (id: number) => {
+    try {
+      loadingContext.setIsLoading(true);
+      const data = await articleAPI.acceptArticle(id);
+      return data;
+    } catch (error: any) {
+      messageContext.onAddMessage(
+        "Something went wrong when accepting the article!"
+      );
+    } finally {
+      loadingContext.setIsLoading(false);
+    }
+  };
+
   const deleteArticle = async () => {};
 
   return (
-    <ArticleContext.Provider value={{ articles, createArticle }}>
+    <ArticleContext.Provider value={{ articles, createArticle, acceptArticle }}>
       {children}
     </ArticleContext.Provider>
   );
