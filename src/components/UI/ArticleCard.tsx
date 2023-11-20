56a57d3fa8a1f214745067895214c205b666ccc4
zhyrzh <arongrhyz@gmail.com>
2023-11-20 20:54:29 +0800

    added accept article functionality

--- src/components/UI/ArticleCard.tsx
+++ src/components/UI/ArticleCard.tsx
@@ -1,6 +1,6 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import dayjs from "dayjs";
-
+import AlertDialog from "./AlertDialog";
 interface IArticleCard {
   id: number;
   img: string;
@@ -18,35 +18,46 @@ const ArticleCard: FC<IArticleCard> = ({
   title,
   onAccept,
 }) => {
+  const [showAcceptModal, setShowAcceptModal] = useState<boolean>(false);
+
   return (
-    <div className="articles-card">
-      <div className="articles-card__img-container">
-        <img src={img} alt="" />
-      </div>
-      <div className="articles-card__content-container">
-        <div>
-          <h1 className="articles-card__title">{title}</h1>
-          <div className="articles-card__secondary-info">
-            <p>{author}</p>
-            <p>{dayjs(date).format("MMMM DD, YYYY").toString()}</p>
-          </div>
+    <>
+      <AlertDialog
+        onAccept={() => {
+          onAccept(id);
+          setShowAcceptModal(false);
+        }}
+        isShowed={showAcceptModal}
+      />
+      <div className="articles-card">
+        <div className="articles-card__img-container">
+          <img src={img} alt="" />
         </div>
-        <div className="articles-card__cta-container">
-          <p className="articles-card__cta-container articles-card__cta-container--bold">
-            MORE INFO
-          </p>
+        <div className="articles-card__content-container">
           <div>
-            <p
-              className="articles-card__cta-container articles-card__cta-container--bold"
-              onClick={() => onAccept(id)}
-            >
-              ACCEPT
+            <h1 className="articles-card__title">{title}</h1>
+            <div className="articles-card__secondary-info">
+              <p>{author}</p>
+              <p>{dayjs(date).format("MMMM DD, YYYY").toString()}</p>
+            </div>
+          </div>
+          <div className="articles-card__cta-container">
+            <p className="articles-card__cta-container articles-card__cta-container--bold">
+              MORE INFO
             </p>
-            <p>DECLINE</p>
+            <div>
+              <p
+                className="articles-card__cta-container articles-card__cta-container--bold"
+                onClick={() => setShowAcceptModal(true)}
+              >
+                ACCEPT
+              </p>
+              <p>DECLINE</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
