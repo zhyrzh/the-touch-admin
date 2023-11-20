2b5005dc20241b420baa758a1e71c7a452702c8c
zhyrzh <arongrhyz@gmail.com>
2023-11-20 20:53:59 +0800

    added new prop isShowed

--- src/components/UI/AlertDialog.tsx
+++ src/components/UI/AlertDialog.tsx
@@ -1,24 +1,29 @@
 import { FC } from "react";
+import { createPortal } from "react-dom";
 interface IAlertDialog {
+  isShowed: boolean;
   onAccept: () => void;
 }
 
-const AlertDialog: FC<IAlertDialog> = ({ onAccept }) => {
-  return (
-    <div className="alert-dialog-backdrop">
-      <div className="alert-dialog">
-        <h1 className="alert-dialog__title">Accept article?</h1>
-        <p className="alert-dialog__description">
-          Are you sure you want to accept selected article? Accepting this
-          article will make it visible to the readers of the site.
-        </p>
-        <div className="alert-dialog__button-container">
-          <button>Cancel</button>
-          <button onClick={() => onAccept()}>Accept</button>
-        </div>
-      </div>
-    </div>
-  );
+const AlertDialog: FC<IAlertDialog> = ({ isShowed, onAccept }) => {
+  return isShowed
+    ? createPortal(
+        <div className="alert-dialog-backdrop">
+          <div className="alert-dialog">
+            <h1 className="alert-dialog__title">Accept article?</h1>
+            <p className="alert-dialog__description">
+              Are you sure you want to accept selected article? Accepting this
+              article will make it visible to the readers of the site.
+            </p>
+            <div className="alert-dialog__button-container">
+              <button>Cancel</button>
+              <button onClick={() => onAccept()}>Accept</button>
+            </div>
+          </div>
+        </div>,
+        document.getElementById("alert-dialog-overlay")!
+      )
+    : null;
 };
 
 export default AlertDialog;
