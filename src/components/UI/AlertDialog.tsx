179e65364088bd8f49fe05ce05bfd376aac0db7c
zhyrzh <arongrhyz@gmail.com>
2023-11-29 22:06:40 +0800

    added new props to make the component dynamic

--- src/components/UI/AlertDialog.tsx
+++ src/components/UI/AlertDialog.tsx
@@ -2,20 +2,25 @@ import { FC } from "react";
 import { createPortal } from "react-dom";
 interface IAlertDialog {
   isShowed: boolean;
+  title: string;
+  description: string;
   onCancel: () => void;
   onAccept: () => void;
 }
 
-const AlertDialog: FC<IAlertDialog> = ({ isShowed, onAccept, onCancel }) => {
+const AlertDialog: FC<IAlertDialog> = ({
+  isShowed,
+  title,
+  description,
+  onAccept,
+  onCancel,
+}) => {
   return isShowed
     ? createPortal(
         <div className="alert-dialog-backdrop">
           <div className="alert-dialog">
-            <h1 className="alert-dialog__title">Accept article?</h1>
-            <p className="alert-dialog__description">
-              Are you sure you want to accept selected article? Accepting this
-              article will make it visible to the readers of the site.
-            </p>
+            <h1 className="alert-dialog__title">{title}</h1>
+            <p className="alert-dialog__description">{description}</p>
             <div className="alert-dialog__button-container">
               <button onClick={() => onCancel()}>Cancel</button>
               <button onClick={() => onAccept()}>Accept</button>
